cbce85afaec042eb3727e0dbfa410722fc9c4f08
natdew <natashadewitz@gmail.com>
2015-10-23 18:03:10 -0400

    leaves falling

--- illustration_p5/illustration.js
+++ illustration_p5/illustration.js
@@ -1,4 +1,4 @@
-// illustration.js
+//illustration.js
 
 //give variables to each image
 var backgroundImage;
@@ -18,9 +18,11 @@ var grass;
 
 var largeCloudX = -100;
 var smallCloudX = 600;
+var leavesFalling = false;
+var leavesY = 200;
+var crownOn = 300;
 
-
-// load the images
+//load the images
 function preload() {
 	backgroundImage = loadImage("images/background.png");
 	smallCloud = loadImage("images/smallCloud.png");
@@ -48,33 +50,57 @@ function draw() {
 	image(backgroundImage, 0, 0);
 
 	//animate the clouds
-			largeCloudX += 0.8;
-			if (largeCloudX > 800) {
-				largeCloudX = -200;
-			}
-			image(largeCloud, largeCloudX, 100);
+	largeCloudX += 0.8;
+	if (largeCloudX > 800) {
+		largeCloudX = -200;
+	}
+	image(largeCloud, largeCloudX, 100);
 
-			smallCloudX += 0.5;
-			if (smallCloudX > 800) {
-				smallCloudX = -200;
-			}
-			image(smallCloud, smallCloudX, 50);
+	smallCloudX += 0.5;
+	if (smallCloudX > 800) {
+		smallCloudX = -200;
+	}
+	image(smallCloud, smallCloudX, 50);
 
-//add the tree, cover up the bottom with grass
-//make leaves fall when tree is clicked on
+	//add the tree
 
-image(tree, 0, 0);
-	// if (mouseIsPressed) {
-	// image(leafFive, 200, 200);
-	// }
+	image(tree, 0, 0);
 
+	//make leaves fall when tree is clicked on
+	if (mouseIsPressed) {
+		if (mouseX < 300) {
+			if (mouseY < 245);
+			leavesFalling = true;
+		}
+	}
+
+	if (leavesFalling) {
+		image(leafFive, 125, leavesY);
+		image(leafOne, 145, leavesY);
+		image(leafThree, 160, leavesY);
+		image(leafSix, 190, leavesY);
+		image(leafSeven, 220, leavesY);
+
+		if (leavesY < 500) {
+			leavesY += 0.6;
+		}
+	}
+
+	//add the grass over the bottom of the tree
 	image(grass, 0, 500);
 
-//insert the character
-image(boy, mouseX - 50, 230);
-	// if (mouseIsPressed){
-		// image(leafCrown, );
-// }
+	//insert the character
+	image(boy, mouseX - 50, 230);
+
+	//add the leaf crown when you click on the boy's head
+	if (mouseIsPressed) {
+		// if (mouseY > 313 && mouseY < 326);
+		crownOn = true
+	}
+
+	if (crownOn) {
+		image(leafCrown, mouseX - 20, 220);
+	}
 
 
 }
